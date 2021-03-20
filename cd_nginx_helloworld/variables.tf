77c2118a2067e0749e750aad0b8f3b0eb086efcd
WS <694337137@qq.com>
2021-03-20 21:58:58 +0800

    Create variables.tf

---
+++ cd_nginx_helloworld/variables.tf
@@ -0,0 +1,21 @@
+variable "replicas_num" {
+  default = "3"
+}
+
+variable "service_name" {
+  default = "nginx"
+}
+
+variable "deployment_port" {
+  default = "80"
+}
+
+variable "service_port" {
+  default = "80"
+}
+
+variable "tag" {
+  default = "v1"
+}
+
+
